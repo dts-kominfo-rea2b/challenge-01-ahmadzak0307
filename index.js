e00215a721d4400f83d9466dadf91286182ef8f3
ahmadzak0307 <ahmadzak0307@nusamandiri.ac.id>
2022-05-29 03:17:32 +0700

    challenge data structure

--- index.js
+++ index.js
@@ -1,11 +1,82 @@
 // TODO: Buatlah data dari kedua user di sini
 // Tentukan tipe data dan struktur data yang menurut kalian tepat
 // Kalian bisa membuat variabel lain untuk dimasukkan ke dalam atribut user agar lebih mudah dibaca
-const firstUser = {};
-const secondUser = {};
+
+
+const educationSet = new Map();
+educationSet.set("firstEdu", [{
+    name: "SD 01",
+    city: "Jakarta",
+    graduate: 2016
+}, {
+    name: "SMP 02",
+    city: "Jakarta",
+    graduate: 2019
+}, {
+    name: "SMA 03",
+    city: "Tangerang"
+}]);
+
+educationSet.set("secondEdu", [{
+    name: "SD 02",
+    city: "Jakarta",
+    graduate: "2010"
+}, {
+    name: "SMP 03",
+    city: "Bogor",
+    graduate: "2013"
+}, {
+    name: "SMA 01",
+    city: "Surabaya",
+    graduate: "2016"
+}, {
+    name: "Universitas Maju",
+    city: "Tangerang"
+}]);
+
+
+const firstUser = {
+    name: "Monica",
+    gender: "Female",
+    age: "17",
+    email: "monica@dingdong.com",
+    favoriteColor: [],
+    isHavePet: "Yes",
+    education: educationSet.get("firstEdu"),
+    favoriteRestaurant: []
+};
+
+const secondUser = {
+    name: "Wendy",
+    gender: "Male",
+    age: "23",
+    email: "wendy@dingdong.com",
+    favoriteColor: [],
+    isHavePet: "No",
+    education: educationSet.get("secondEdu"),
+    favoriteRestaurant: []
+};
+
+firstUser.favoriteColor = new Set(["Yellow",
+    "Pink",
+    "White",
+    "Purple"
+]);
+
+secondUser.favoriteColor = new Set(["Blue",
+    "Black",
+    "Grey"
+]);
+
+firstUser.favoriteRestaurant = new Set(["Bento", "Sushi", "Pancake", "Eggy", "Tempura", "Bento", "Eggy", "Padang", "Tteok", "Sushi", "Sushi"]);
+secondUser.favoriteRestaurant = new Set(["Tempura", "Bento", "Sushi", "Pancake", "Padang", "Katsu", "Geprek", "Pancake", "Eggy"]);
 
 // TODO: Masukkan hasil akhir dari kedua user di sini
 const users = [];
+users.push(firstUser)
+users.push(secondUser)
+    // console.log(JSON.stringify(users, null, 2));
+
 
 // ! JANGAN MODIFIKASI LINE DI BAWAH
 function main() {
